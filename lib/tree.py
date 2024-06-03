4f10a36082e9f4d59e2efb4ce7032db72973f1e1
jeff muriuki <jeff.muriuki@studet.moringaschool.com>
2024-06-03 11:19:10 +0300

    initial

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,16 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    return self._dfs(self.root, id)
+  
+  def _dfs(self, node, id):
+    if node is None:
+        return None
+    if node.get('id') == id:
+        return node
+    for child in node.get( 'children', []):
+          result = self._dfs(child, id)
+          if result is not None:
+            return result
+    return None
+    
